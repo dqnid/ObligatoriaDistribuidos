5515c61c504875f4493c33577fd7388d5084a033
dqnid <dani.heras@hotmail.com>
2023-04-30 15:09:17 +0200

    clear

--- bin/mergeLogs.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cat ./log/*FinalAdjusted.log > ./log/total.log
-sort -k 3 ./log/total.log > ./ssdd.log
--- bin/stop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for dir in ./tomcat/*/
-do
-	chmod +x $dir/bin/*
-	$dir/bin/shutdown.sh
-done
